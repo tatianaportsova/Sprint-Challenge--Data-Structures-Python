ce85bfe56d317452c0822c995b3ada50f8b25209
tatianaportsova <tt.sova@gmail.com>
2020-08-14 13:10:52 -0400

    Completed sprint challenge

--- names/names.py
+++ names/names.py
@@ -2,6 +2,36 @@ import time
 
 start_time = time.time()
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+               self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if self.right is None:
+               self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -12,15 +42,25 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+names_1_tree = BSTNode(names_1[0])
+
+for i in range(1, len(names_1)):
+    names_1_tree.insert(names_1[i])
+
+for i in names_2:
+    if names_1_tree.contains(i):
+        duplicates.append(i)
+
 # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-duplicates = list(set(names_1) & set(names_2))
+# duplicates = list(set(names_1) & set(names_2))
 
 end_time = time.time()
+
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
@@ -28,3 +68,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# duplicates = list(set(names_1) & set(names_2))
